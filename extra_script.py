2b9692c4fa0dd25a90e88df7e516a0527f3472a1
Luc <8822552+luc-github@users.noreply.github.com>
2020-10-17 16:35:41 +0200

    try again

--- extra_script.py
+++ extra_script.py
@@ -1,2 +1,4 @@
 import shutil
-shutil.copytree("../src", ".pio/libdeps/esp32/ESP3DLib/")
+shutil.rmtree("Marlin/.pio/libdeps/esp32/ESP3DLib/")
+shutil.copytree("../src", "Marlin/.pio/libdeps/esp32/ESP3DLib/")
+print("Update ESP3DLib files")
