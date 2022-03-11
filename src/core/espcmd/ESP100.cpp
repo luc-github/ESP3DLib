bd8f3eaf8c451ecd86c8beba662cdaa324d06dbb
Luc <8822552+luc-github@users.noreply.github.com>
2022-03-11 09:56:50 +0800

    Update ESP100.cpp
    Fix wrong code for authentication failed

--- src/core/espcmd/ESP100.cpp
+++ src/core/espcmd/ESP100.cpp
@@ -24,6 +24,7 @@
 #include "../settings_esp3d.h"
 #include "../../modules/wifi/wificonfig.h"
 #include "../../modules/authentication/authentication_service.h"
+#define COMMANDID   100
 //STA SSID
 //[ESP100]<SSID>[json=yes][pwd=<admin password>]
 bool Commands::ESP100(const char* cmd_params, level_authenticate_type auth_type, ESP3DOutput * output)
@@ -32,10 +33,13 @@ bool Commands::ESP100(const char* cmd_params, level_authenticate_type auth_type,
     bool json = has_tag (cmd_params, "json");
     String response;
     String parameter;
+    uint8_t errorCode = 501;
+
 #ifdef AUTHENTICATION_FEATURE
     if (auth_type == LEVEL_GUEST) {
-        response = format_response(100, json, false, "Guest user can't use this command");
+        response = format_response(COMMANDID, json, false, "Guest user can't use this command");
         noError = false;
+        errorCode = 401;
     }
 #else
     (void)auth_type;
@@ -44,24 +48,25 @@ bool Commands::ESP100(const char* cmd_params, level_authenticate_type auth_type,
         parameter = clean_param(get_param (cmd_params, ""));
         //get
         if (parameter.length() == 0) {
-            response = format_response(100, json, true, Settings_ESP3D::read_string(ESP_STA_SSID));
+            response = format_response(COMMANDID, json, true, Settings_ESP3D::read_string(ESP_STA_SSID));
         } else { //set
 #ifdef AUTHENTICATION_FEATURE
             if (auth_type != LEVEL_ADMIN) {
-                response = format_response(100, json, false, "Wrong authentication level");
+                response = format_response(COMMANDID, json, false, "Wrong authentication level");
                 noError = false;
+                errorCode = 401;
             }
 #endif //AUTHENTICATION_FEATURE
             if (noError) {
                 if (!WiFiConfig::isSSIDValid (parameter.c_str() ) ) {
-                    response = format_response(100, json, false, "Incorrect SSID");
+                    response = format_response(COMMANDID, json, false, "Incorrect SSID");
                     noError = false;
                 } else {
                     if(!Settings_ESP3D::write_string(ESP_STA_SSID, parameter.c_str())) {
-                        response = format_response(100, json, false, "Set failed");
+                        response = format_response(COMMANDID, json, false, "Set failed");
                         noError = false;
                     } else {
-                        response = format_response(100, json, true, "ok");
+                        response = format_response(COMMANDID, json, true, "ok");
                     }
                 }
             }
@@ -74,7 +79,7 @@ bool Commands::ESP100(const char* cmd_params, level_authenticate_type auth_type,
             output->printMSG (response.c_str() );
         }
     } else {
-        output->printERROR(response.c_str(), 401);
+        output->printERROR(response.c_str(), errorCode);
     }
     return noError;
 }
