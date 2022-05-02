96c35c077eccdea9030a53e31408524656e82648
Luc <8822552+luc-github@users.noreply.github.com>
2022-05-03 07:41:39 +0800

    Update esp3dlib_config.h

--- src/esp3dlib_config.h
+++ src/esp3dlib_config.h
@@ -186,6 +186,8 @@
 //let -1 or comment if none
 #if defined (SD_DETECT_PIN) && (SD_DETECT_PIN > 0)
 #define ESP_SD_DETECT_PIN       SD_DETECT_PIN
+#else
+#define ESP_SD_DETECT_VALUE  LOW
 #endif //SD_DETECT_PIN  
 
 //value expected for ESP_SD_DETECT_PIN
